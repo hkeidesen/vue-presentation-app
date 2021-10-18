f636229bdfd6dcb5e3f67ec7ba3e8b06cb979bf4
Norum Eidesen <Hans.Kristian.Eidesen@dnv.com>
2021-10-18 14:25:24 +0200

    moved files to root

---
+++ public/todo.md
@@ -0,0 +1,6 @@
+Todos:
+- Create a function that determines what index in the weather API call that will be used to get the weather icon.
+- Periodically fetch new weather and render to DOM with new data (do I need to use getters and setters here?)
+- Create a "weather-now" object. 
+
+Done:
---
+++ src/components/WeatherGraph.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- use a function to determine the max and min bound  -->
+  <la-cartesian
+    :width="750"
+    :height="140"
+    :bound="[-1, (n) => n + 2]"
+    :data="temperatureDataToPlot"
+  >
+    <la-area prop="temperature" 
+    animated
+    curve="curveBumpX"
+    color="#1890ff">
+      <!-- <la-area
+      prop="temperature"
+      color="goldenrod"
+      animated="true"
+      curve="curveBumpX"
+      :width="2"
+    > -->
+      <g slot-scope="props" :fill="props.color">
+        <!-- <rect
+          :x="props.x - 5"
+          :y="props.y - 5"
+          width="5"
+          height="5"
+          >
+        </rect> -->
+        <text :x="props.x" :y="props.y" text-anchor="middle" dy="-.5em">
+          {{ props.value }}
+        </text>
+      </g>
+    </la-area>
+    <la-x-axis prop="time" :tickSize="5"></la-x-axis>
+  </la-cartesian>
+</template>
+
+<script>
+import { Cartesian, Area, XAxis } from "laue"; //see https://laue.js.org
+
+export default {
+  name: "WeatherGraph",
+  props: {
+    temperatureDataToPlot: {
+      type: Array,
+    },
+  },
+  components: {
+    LaCartesian: Cartesian,
+    LaArea: Area,
+    LaXAxis: XAxis,
+  },
+//since the data is being passed from the parent as a prop, there is no need to declare the data is data(){return{data}}!
+};
+</script>
+
+<style scoped></style>
